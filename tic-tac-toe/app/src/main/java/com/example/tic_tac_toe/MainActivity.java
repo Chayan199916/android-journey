f4e1c406df680bf8f6cf9170c64fb5f5e2296dde
Chayan Kumar Sengupta <swapnomoy199916@gmail.com>
2020-07-01 02:47:55 +0530

    version 1.1 released | series playing functionality added

--- tic-tac-toe/app/src/main/java/com/example/tic_tac_toe/MainActivity.java
+++ tic-tac-toe/app/src/main/java/com/example/tic_tac_toe/MainActivity.java
@@ -12,11 +12,18 @@ import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     boolean gameValid = true;
+    String winTeam;
+    int player1_score = 0;
+    int player2_score = 0;
     int curPosOfGame[] = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     int activePlayer = 0;
     int validWinPositions[][] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
     public void tap_to_play(View view){
         int flag = 1;
+        TextView player1 = findViewById(R.id.score1);
+        TextView player2 = findViewById(R.id.score2);
+        player1.setText("Player 1 : " + Integer.toString(player1_score));
+        player2.setText("Player 2 : " + Integer.toString(player2_score));
         if(!gameValid){
             gameReset();
         }
@@ -39,12 +46,28 @@ public class MainActivity extends AppCompatActivity {
             img.animate().translationYBy(500f).setDuration(70);
             for(int i = 0; i < 8; i++){
                 if(curPosOfGame[validWinPositions[i][0]] == curPosOfGame[validWinPositions[i][1]] && curPosOfGame[validWinPositions[i][1]] == curPosOfGame[validWinPositions[i][2]] && curPosOfGame[validWinPositions[i][1]] != 2){
-                   String winTeam = curPosOfGame[validWinPositions[i][0]] == 0 ? "Player 1" : "Player 2";
+                   winTeam = curPosOfGame[validWinPositions[i][0]] == 0 ? "Player 1" : "Player 2";
                    Toast.makeText(this, "GAME OVER !", Toast.LENGTH_SHORT).show();
                    status.setText(winTeam + " has won, Congrats!");
+                    if(winTeam.equals("Player 1")){
+                        player1_score++;
+                        player1.setText("Player 1 : " + Integer.toString(player1_score));
+                    }else{
+                        player2_score++;
+                        player2.setText("Player 2 : " + Integer.toString(player2_score));
+                    }
                    gameValid = false;
                    break;
                 }
+            }
+            if(player1_score == 2){
+                Toast.makeText(this, "SERIES OVER !", Toast.LENGTH_SHORT).show();
+                status.setText("Player 1 has won the series, Congrats!");
+                player1_score = player2_score = 0;
+            }else if (player2_score == 2){
+                Toast.makeText(this, "SERIES OVER !", Toast.LENGTH_SHORT).show();
+                status.setText("Player 2 has won the series, Congrats!");
+                player1_score = player2_score = 0;
             }
             for(int i = 0; i < curPosOfGame.length; i++){
                 if(curPosOfGame[i] == 2){
